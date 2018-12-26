2c36a01b63646419f1e875b2dc69ee436c024b48
Vlad <vlad.miroshnichenko@agilie.com>
2018-12-26 18:23:22 +0200

    add opacity

--- bubble-range.js
+++ bubble-range.js
@@ -12,7 +12,8 @@ var BubbleRange = function() {
             backgroundColorControl: this.dataRange.backgroundColorControl,
             parent: self.bubble,
             size: self.size,
-            indent: self.dataRange.indent
+            indent: self.dataRange.indent,
+            opacity: self.dataRange.opacity
         };
         // create range
         var range = new Range(dataRange);
@@ -25,36 +26,36 @@ var BubbleRange = function() {
         this.bubble.style.width = this.dataRange.size;
         this.bubble.style.height = this.dataRange.size;
         this.bubble.style.backgroundColor = this.dataRange.backgroundColor;
+        this.bubble.style.opacity = this.dataRange.opacity;
         container.appendChild(this.bubble);
     };
 
      function Range(data) {
          this.data = data;
-         this.container = data.parent;
-         this.containerSize = data.size;
-         this.indent = data.indent;
-         this.bobbleRangeControl = undefined;
+         this.bobbleRangeControl;
          this.init();
      }
 
     Range.prototype.init = function () {
         this.bobbleRangeControl = document.createElement('div');
         this.bobbleRangeControl.classList.add('bubble-range__control');
-        this.bobbleRangeControl.style.width = setSizeControl(this.containerSize) + 'px';
-        this.bobbleRangeControl.style.height = setSizeControl(this.containerSize)+ 'px';
+        this.bobbleRangeControl.style.width = setSizeControl(this.data.size) + 'px';
+        this.bobbleRangeControl.style.height = setSizeControl(this.data.size)+ 'px';
         this.bobbleRangeControl.style.backgroundColor = this.data.backgroundColorControl;
-        this.container.appendChild(this.bobbleRangeControl);
+        this.data.parent.appendChild(this.bobbleRangeControl);
         this.addEvents();
+        this.addEventsMobile();
     };
 
     Range.prototype.addEvents = function () {
-        var heightElement = this.bobbleRangeControl.clientHeight;
+        var heightElement = setSizeControl(this.data.size);
         var startPoint = 0;
         var isDown = false;
         var self = this;
+        var differentOpacity = 1 - this.data.opacity;
 
         function mouseDown() {
-            // addClassElement(self.bobbleRangeControl, 'bubble-range__control--active');
+            addClassElement(self.bobbleRangeControl, 'bubble-range__control--active');
             isDown = true;
         }
         function mouseUpLeave() {
@@ -63,37 +64,67 @@ var BubbleRange = function() {
         }
         function mouseMove(event) {
             if (isDown) {
-                addClassElement(self.bobbleRangeControl, 'bubble-range__control--active');
                 if (!startPoint) {
                     startPoint = event.clientY;
                 }
                 if (startPoint >= event.clientY) {
                     var currentHeight = startPoint - event.clientY + heightElement;
-                    if (validateHeight(currentHeight)) {
+                    if (validateHeight(currentHeight, self.data.size, self.data.indent)) {
                         self.bobbleRangeControl.style.width = currentHeight + 'px';
                         self.bobbleRangeControl.style.height = currentHeight + 'px';
+                        self.changeOpacity(self.showRangeValue(Number(currentHeight)), differentOpacity);
                         self.showRangeValue(currentHeight);
-                        console.log('showRangeValue', self.showRangeValue(Number(currentHeight)));
+                        BubbleRange.test = self.showRangeValue(currentHeight);
+                        // console.log('showRangeValue', self.showRangeValue(Number(currentHeight)));
                     }
                 }
-            }
-        }
-        function validateHeight(height) {
-            if (height <= self.containerSize - self.indent) {
-                return true;
             }
         }
 
         this.bobbleRangeControl.addEventListener('mousedown', mouseDown);
         this.bobbleRangeControl.addEventListener('mouseup', mouseUpLeave);
         this.bobbleRangeControl.addEventListener('mousemove', mouseMove);
-        this.container.addEventListener('mousemove', mouseMove);
-        this.container.addEventListener('mouseleave', mouseUpLeave);
+        this.data.parent.addEventListener('mousemove', mouseMove);
+        this.data.parent.addEventListener('mouseleave', mouseUpLeave);
+    };
+
+    Range.prototype.addEventsMobile = function() {
+        var heightElement = setSizeControl(this.data.size);
+        var startPoint = 0;
+        var self = this;
+        var differentOpacity = 1 - this.data.opacity;
+
+        function touchMove(event) {
+            // console.log('event', event);
+            if (!startPoint) {
+                startPoint = event.targetTouches[0].pageY;
+            }
+            if (startPoint >= event.targetTouches[0].pageY) {
+                var currentHeight = startPoint - event.targetTouches[0].pageY + heightElement;
+                console.log('currentHeight', currentHeight);
+                if (validateHeight(currentHeight, self.data.size, self.data.indent)) {
+                    self.bobbleRangeControl.style.width = currentHeight + 'px';
+                    self.bobbleRangeControl.style.height = currentHeight + 'px';
+                    self.showRangeValue(currentHeight);
+                    // console.log('showRangeValue', self.showRangeValue(Number(currentHeight)));
+                }
+            }
+        }
+
+        this.bobbleRangeControl.addEventListener('touchmove', touchMove);
     };
 
     Range.prototype.showRangeValue = function (currentSize) {
-       return (currentSize - setSizeControl(this.containerSize)) * 100 / (this.containerSize - setSizeControl(this.containerSize) - this.indent);
+       return (currentSize - setSizeControl(this.data.size)) * 100 / (this.data.size - setSizeControl(this.data.size) - this.data.indent);
     };
+
+    Range.prototype.changeOpacity = function (currentValue, differentOpacity) {
+        console.log('currentValue', currentValue);
+        var opacityValue = differentOpacity * currentValue / 100;
+        console.log('opacityValue', opacityValue);
+        this.data.parent.style.opacity = this.data.opacity + opacityValue;
+    };
+
 
     var validateBubble = {
         size: function (data) {
@@ -114,6 +145,11 @@ var BubbleRange = function() {
         indent: function (data) {
             if ( !data.indent) {
                 data.indent = 10;
+            }
+        },
+        opacity: function (data) {
+            if ( !data.opacity) {
+                data.opacity = 0.9;
             }
         }
     };
@@ -138,6 +174,11 @@ var BubbleRange = function() {
         return size * 20 / 100;
     }
 
+    function validateHeight(height, sizeElement, indent) {
+        if (height <= sizeElement - indent) {
+            return true;
+        }
+    }
 
     return BubbleRange;
 }();
--- common.js
+++ common.js
@@ -1,3 +1,10 @@
 var range = new BubbleRange({
-    wrapper: '.test'
+    wrapper: '.test',
+    opacity: 0.5
 });
+
+console.log(range.test);
+
+setTimeout( function () {
+    console.log(BubbleRange.test);
+}, 5000 );
